638f8a412355517969fc9a9e7c1cd36c4896b72a
Charles Davenport <cdavenport090@gmail.com>
2021-09-05 21:17:12 -0400

    Organize files and fix broken dependencies

---
+++ Player/Player.gd
@@ -0,0 +1,113 @@
+extends KinematicBody
+
+export var move_speed: float = 10.0
+export var look_sens: float = .01
+export var accel: float = 5.0
+export var deaccel: float = 10.0
+export var fall_accel_down: float = 3.0
+export var fall_accel_up: float = 1.0
+export var max_fall_speed: float = -15.0
+export var jump_speed: float = 10.0
+
+var vel: Vector3
+var move_input: Vector3
+var snap: Vector3
+
+var is_jump_cooldown: bool
+var jump_request: bool
+var is_returning_weapon_center: bool
+
+onready var cam = get_node("Camera")
+onready var jump_timer = get_node("JumpTimer")
+onready var molotov_scene = preload("res://Molotov/MolotovProjectile.tscn")
+onready var molotov_spawn = get_node("Camera/MolotovSpawn")
+
+func _ready():
+	vel = Vector3.ZERO
+	move_input = Vector3.ZERO
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	is_jump_cooldown = false
+	jump_timer.connect("timeout", self, "on_jump_cooldown")
+	is_returning_weapon_center = false
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		var mouse_delta = event.relative
+		cam.rotate_x(mouse_delta.y * look_sens )
+		self.rotate_y(-1 * mouse_delta.x * look_sens)
+
+func _process(_delta):
+	get_move_input()
+
+func _physics_process(delta):
+	snap = Vector3.DOWN
+	if is_on_floor():
+		vel.y = 0.0
+		snap = -get_floor_normal()
+	else:
+		snap = Vector3.ZERO
+	#apply_slope()
+	apply_move_acceleration(delta)
+	if jump_request:
+		apply_jump()
+	elif not is_on_floor():
+		apply_gravity(delta)
+	#vel = move_and_slide(move_input, Vector3.UP, true, 4, 1.22)
+	vel = move_and_slide_with_snap(move_input, snap, Vector3.UP, true, 4)
+#	var slides = get_slide_count()
+#	if slides > 0:
+#		handle_slopes(slides)
+
+func apply_slope() -> void:
+	# rotate movement input to be on the plane of a slope
+	var slides = get_slide_count()
+	if slides == 0:
+		return
+	for i in range(slides):
+		var col = get_slide_collision(i)
+		if col.normal.y < 1.0:
+			pass
+
+func apply_move_acceleration(delta: float) -> void:
+	var wish_dir = move_input * move_speed
+	if wish_dir.dot(move_input) > 0:
+		move_input = vel.linear_interpolate(wish_dir, accel*delta)
+	else:
+		move_input = vel.linear_interpolate(wish_dir, deaccel*delta)
+
+func apply_jump() -> void:
+	move_input.y += jump_speed
+	jump_request = false
+	snap = Vector3.ZERO
+	is_jump_cooldown = true
+	jump_timer.start()
+
+func apply_gravity(delta) -> void:
+	if vel.y > 0:
+		move_input.y = lerp(vel.y, max_fall_speed, fall_accel_up*delta)
+	else:
+		move_input.y = lerp(vel.y, max_fall_speed, fall_accel_down*delta)
+
+func on_jump_cooldown() -> void:
+	jump_timer.stop()
+	is_jump_cooldown = false
+
+func get_move_input() -> void:
+	var input_vec = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		input_vec += self.transform.basis.z
+	if Input.is_action_pressed("back"):
+		input_vec -= self.transform.basis.z
+	if Input.is_action_pressed("left"):
+		input_vec += self.transform.basis.x
+	if Input.is_action_pressed("right"):
+		input_vec -= self.transform.basis.x
+	move_input = input_vec.normalized()
+	if Input.is_action_just_pressed("jump") and not is_jump_cooldown:
+		jump_request = true
+
+func spawn_molotov(speed: float) -> void:
+	var player_forward_vel = vel.dot(self.transform.basis.z.normalized())
+	var molotov_inst = molotov_scene.instance()
+	get_tree().root.add_child(molotov_inst)
+	molotov_inst.start(molotov_spawn.global_transform, speed, player_forward_vel)
---
+++ Player/WeaponManager.gd
@@ -0,0 +1,106 @@
+extends Node
+
+onready var player = get_parent()
+onready var handAnim = get_parent().get_node("HandAnimator")
+onready var weaponSwayAnim = get_parent().get_node("WeaponSwayAnimator")
+onready var handsNode = get_parent().get_node("PlayerHUD/Hands")
+onready var playerHUD = get_parent().get_node("PlayerHUD")
+
+var is_returning_weapon_center: bool
+var molotov_charge: float
+
+func _ready():
+	handAnim.play("molotov_equip")
+	is_returning_weapon_center = false
+	molotov_charge = 0.0
+	
+func _process(delta):
+	process_weapon_sway()
+	process_hand_anim()
+
+func process_hand_anim() -> void:
+	if handAnim.current_animation == "molotov_idle" and Input.is_action_just_pressed("fire"):
+		handAnim.stop()
+		handAnim.play("light_molotov") # -> on_light_molotov_end()
+
+# ********************************************************************
+# FIRE MOLOTOV
+# ********************************************************************
+func molotov_fire() -> void:
+	handAnim.stop()
+	handAnim.play("throw_molotov_hold")
+	playerHUD.molotovChargeBar.visible = true
+	var co = charge_molotov_throw()
+	while co is GDScriptFunctionState and co.is_valid():
+		co = yield(co, "completed")
+	playerHUD.molotovChargeBar.visible = false
+	player.spawn_molotov(molotov_charge)
+	handAnim.stop()
+	handAnim.play("throw_molotov")
+	molotov_charge = 0.0
+
+func charge_molotov_throw() -> void:
+	while true:
+		if Input.is_action_just_released("fire"):
+			return
+		else:
+			molotov_charge += 1.0 # should multiply by delta time?
+			molotov_charge = clamp(molotov_charge, 0.0, 100.0) 
+			playerHUD.molotovChargeBar.value = molotov_charge
+			yield(get_tree(), "idle_frame")
+
+# ********************************************************************
+# WEAPON SWAY
+# ********************************************************************
+func process_weapon_sway() -> void:
+	# horizontal speed
+	var vel = get_parent().vel
+	var speed = Vector2(vel.x, vel.z).length()
+	if speed > 0.1:
+		if weaponSwayAnim.current_animation != "weapon_sway":
+			weaponSwayAnim.play("weapon_sway")
+		weaponSwayAnim.set_speed_scale(clamp(speed / 10, 0.25, 1.0)) # 10 = hardcoded move_speed from player.gd
+		#print(speed/move_speed)
+	elif weaponSwayAnim.current_animation == "weapon_sway" and not is_returning_weapon_center:
+		is_returning_weapon_center = true
+		#weaponSwayAnim.set_speed_scale(0.5)
+#		$WeaponSwayAnimator.stop()
+		var co = wait_for_weapon_to_center()
+		while co is GDScriptFunctionState and co.is_valid():
+			co = yield(co, "completed")
+		weaponSwayAnim.stop()
+		weaponSwayAnim.play("still")
+		is_returning_weapon_center = false
+
+func return_weapon_to_center() -> void:
+	var target = Vector2(512,600)
+	while handsNode.rect_position.distance_to(target) > 1.0:
+		handsNode.rect_position = handsNode.rect_position.linear_interpolate(target, 0.25)
+		#print(handsNode.rect_position)
+		yield(get_tree(), "idle_frame")
+		
+func wait_for_weapon_to_center() -> void:
+	var target = Vector2(512,600)
+	while handsNode.rect_position.distance_to(target) > 1.0:
+		yield(get_tree(), "idle_frame")
+
+# ********************************************************************
+# ANIMATION PLAYER 
+# ********************************************************************
+func on_light_molotov_end() -> void:
+	handAnim.stop()
+	if Input.is_action_pressed("fire"):
+		molotov_fire()
+	else:
+		handAnim.play("throw_molotov")
+		player.spawn_molotov(0.0)
+
+func on_throw_molotov_end() -> void:
+	handAnim.stop()
+	handAnim.play("molotov_idle")
+	if Input.is_action_pressed("fire"):
+		handAnim.play("light_molotov")
+	
+func on_molotov_equip_end() -> void:
+	handAnim.stop()
+	handAnim.play("molotov_idle")
